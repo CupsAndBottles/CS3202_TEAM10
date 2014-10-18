5ebe239971626cf7073550b77be07222ad20f7af
Joanne Mah <joannemah.j.w@gmail.com>
2014-10-18 17:48:22 +0800

    Added misc files generated from unit testing.
    Issue: Unable to find or open the PDB file

---
+++ EmptyGeneralTesting/UnitTesting/TestVarTable.cpp
@@ -0,0 +1,30 @@
+#include <cppunit\config\SourcePrefix.h>
+#include "VarTable.h"
+#include "TestVarTable.h"
+
+#include <iostream>
+#include <string>
+
+void VarTableTest::setUp() {
+	// empty body
+}
+
+void VarTableTest::tearDown() {
+	// empty body
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(VarTableTest);
+
+// method to test the constructor
+void VarTableTest::testConstructor() {
+	// create a VarTable object
+	VarTable varTableTest;
+
+	int size = varTableTest.getSize();
+	CPPUNIT_ASSERT_EQUAL(0, size);
+	 
+}
+
+
+// insert other methods below here
+// add a comment before each method to indicate its purpose
---
+++ EmptyGeneralTesting/UnitTesting/TestVarTable.h
@@ -0,0 +1,17 @@
+#define TestVarTable_h
+
+#include<cppunit\extensions\HelperMacros.h>
+
+class VarTableTest : public CPPUNIT_NS::TestFixture {
+CPPUNIT_TEST_SUITE(VarTableTest);
+CPPUNIT_TEST(testConstructor);
+// add other methods to test here
+CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+
+	void testConstructor();
+
+};

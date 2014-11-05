3510b3a81bd9daf4bb04d91a087843f3c2564234
CBike1103 <402220265@qq.com>
2014-11-05 13:48:36 +0800

    Added Modifies and Uses again.
    Have some problems with "static" things.

--- EmptyGeneralTesting/SPA/QueryProcessor/QueryEvaluator.h
+++ EmptyGeneralTesting/SPA/QueryProcessor/QueryEvaluator.h
@@ -6,10 +6,10 @@ using namespace std;
 class QueryEvaluator
 {
 private:
-	vector<vector<Synonym>> checkModifies(SuchThatClause,vector<SelectClause>,vector<Declaration>);
+	vector<vector<Synonym>> checkModifies(SuchThatClause, vector<SelectClause>, vector<Declaration>);
 	vector<vector<Synonym>> checkUses(SuchThatClause, vector<SelectClause>, vector<Declaration>);
 	vector<vector<Synonym>> mergeResult(vector<vector<Synonym>>);//will have more parameters
-	Synonym convertIntToSynonym(int,SynonymType);
+	Synonym convertIntToSynonym(int, SynonymType);
 	vector<vector<int>> getAllPossibleResult(vector<SelectClause>);
 
 public:

2613bfe9511138f270ffd6a31ce0739bb2ffdebe
Matthew Saw <sawmatthew@gmail.com>
2014-11-06 12:49:37 +0800

    reinserted files

---
+++ EmptyGeneralTesting/IntegrationTesting/ParserToPKBTest.h
@@ -0,0 +1,20 @@
+#define ParserToPKBTest_h
+
+#include <cppunit\extensions\HelperMacros.h>
+
+class ParserToPKBTest : public CPPUNIT_NS::TestFixture {
+	CPPUNIT_TEST_SUITE(ParserToPKBTest);
+
+	CPPUNIT_TEST(testNothing);
+	// insert methods in here
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+
+	void testNothing();
+
+	// add other methods to test here
+
+};

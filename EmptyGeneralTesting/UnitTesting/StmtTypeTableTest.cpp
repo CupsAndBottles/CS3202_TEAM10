bc1295fb5b12a814233457f5f523d07bd4a9af60
williamkimsha <williamkimsha@hotmail.com>
2015-02-04 04:45:19 +0800

    stmtTypeTable test v1

---
+++ EmptyGeneralTesting/UnitTesting/StmtTypeTableTest.cpp
@@ -0,0 +1,48 @@
+#include <cppunit\config\SourcePrefix.h>
+
+#include "..\SPA\PKB\StmtTypeTable.h"
+#include "StmtTypeTableTest.h"
+
+#include <iostream>
+#include <string>
+#include <stdint.h>
+
+
+void StmtTypeTableTest::setUp() {
+	StmtTypeTable::ClearData();
+}
+
+void StmtTypeTableTest::tearDown() {
+	StmtTypeTable::ClearData();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(StmtTypeTableTest);
+
+void GetAllStmtsOfTypeTest() {
+	StmtTypeTable::ClearData();
+
+	StmtTypeTable::Insert(1, ASSIGN);
+	StmtTypeTable::Insert(2, WHILE);
+	
+	vector<int> resultTest = StmtTypeTable::GetAllStmtsOfType(ASSIGN);
+	CPPUNIT_ASSERT_EQUAL(1, resultTest[0]);
+}
+
+void CheckIfStmtOfTypeTest() {
+	StmtTypeTable::ClearData();
+
+	StmtTypeTable::Insert(1, ASSIGN);
+	StmtTypeTable::Insert(2, WHILE);
+
+	CPPUNIT_ASSERT(StmtTypeTable::CheckIfStmtOfType(1, ASSIGN)); 
+	CPPUNIT_ASSERT(StmtTypeTable::CheckIfStmtOfType(2, WHILE)); 
+}
+
+void InsertTest() {
+	StmtTypeTable::ClearData();
+	
+	StmtTypeTable::Insert(1, ASSIGN);
+
+	vector <int> resultTestInsert = StmtTypeTable::GetAllStmtsOfType(ASSIGN);
+	CPPUNIT_ASSERT(!(resultTestInsert.empty())); 
+}

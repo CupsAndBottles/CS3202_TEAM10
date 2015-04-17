79459fb5f639dbc230bdb13f94316bff5e8bd821
darrenbkl <darrenbkl@gmail.com>
2015-04-17 13:06:22 +0800

    ...

--- EmptyGeneralTesting/IntegrationTesting/UltimateTest2.cpp
+++ EmptyGeneralTesting/IntegrationTesting/UltimateTest2.cpp
@@ -185,25 +185,4 @@ void UltimateTest2::TestWith()
 
 	
 
-	actualResultList.push_back("5");
-
-	qe.PrintIntermediateResult();
-
-	CPPUNIT_ASSERT(resultList == actualResultList);
-
-	query = "assign a; Select BOOLEAN with 1=2";
-
-
-	CPPUNIT_ASSERT_MESSAGE("Query is valid", qv.ValidateQuery(query, qd));
-	CPPUNIT_ASSERT_MESSAGE("Query is successfully evaluated", qe.EvaluateQuery(qd, resultList));
-	qe.PrintIntermediateResult();
-	CPPUNIT_ASSERT_EQUAL_MESSAGE("Number of results is correct", 1, int(resultList.size()));
-
-	
-
-	actualResultList.push_back("false");
-
-	qe.PrintIntermediateResult();
-
-	CPPUNIT_ASSERT(resultList == actualResultList);
 }
